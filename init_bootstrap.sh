791a2d25b5279c7e73c8b89fd1fbddfc8bd0242b
Florian Mounier <florian.mounier@kozea.fr>
2013-12-12 15:11:48 +0100

    Add bootstrap js and fonts too

--- init_bootstrap.sh
+++ init_bootstrap.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sass-convert -R -F scss -T sass ext/sass-bootstrap/lib/ project/static/sass/
+sass-convert -R -F scss -T sass ext/sass-bootstrap/lib project/static/sass
+cp -vf ext/sass-bootstrap/dist/js/bootstrap.min.js project/static/javascripts/
+cp -vf ext/sass-bootstrap/dist/fonts/glyphicons-halflings-regular.* project/static/fonts/
