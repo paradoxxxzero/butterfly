6d0b5f79c3425e67ede983bfc946bbdae839315c
Florian Mounier <florian.mounier@kozea.fr>
2014-01-30 13:35:41 +0100

    Clean up

--- init_bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sass-convert -R -F scss -T sass ext/sass-bootstrap/lib app/static/sass/bootstrap
-cp -vf ext/sass-bootstrap/dist/js/bootstrap.min.js app/static/javascripts/
-cp -vf ext/sass-bootstrap/dist/fonts/glyphicons-halflings-regular.* app/static/stylesheets/fonts/
