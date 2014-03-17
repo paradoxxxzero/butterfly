515a2c6b469ba7798ad7a4aff14e0bf17afcd763
Florian Mounier <florian.mounier@kozea.fr>
2014-03-17 11:49:39 +0100

    Bump 1.3.0

--- butterfly/__init__.py
+++ butterfly/__init__.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '1.2.9'
+__version__ = '1.3.0'
 
 
 import os
