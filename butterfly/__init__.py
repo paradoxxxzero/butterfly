6663568500f79c188e83740f4ed00cba41ac8e3b
Florian Mounier <florian.mounier@kozea.fr>
2017-02-13 15:23:31 +0100

    Version to beta

--- butterfly/__init__.py
+++ butterfly/__init__.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '3.0.0-alpha'
+__version__ = '3.0.0-beta-1'
 
 
 import os
